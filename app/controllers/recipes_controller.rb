6a48fb66333b89d40557089e9232d7dd0a664899
mbalsamo <github email address>
2018-12-29 15:45:25 -0500

    updates recipe show page, creates welcome page, prepares to create new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,14 @@ class RecipesController < ApplicationController
   def index
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
     @ingredients = @recipe.ingredients
