bfeacf5ded945afbc4ed02f24ebcbbe2ba6b1b71
mbalsamo <github email address>
2018-12-29 16:49:29 -0500

    creates new recipe form and allows the creation of new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,12 +8,22 @@ class RecipesController < ApplicationController
   end
 
   def create
-
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.valid?
+      @recipe.save
+      redirect_to @recipe
+    end
   end
 
   def show
     @recipe = Recipe.find(params[:id])
     @ingredients = @recipe.ingredients
     @instructions = @recipe.instructions
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :yeild, :cook_time)
   end
 end
