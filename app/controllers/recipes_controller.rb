7a6e819e1fc40d6c645230f89ae0b2b9086ae799
mbalsamo <github email address>
2019-01-21 13:46:51 -0500

    adds edit and update actions and logic, updates index action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,8 @@
 class RecipesController < ApplicationController
+  before_action :current_recipe, only: %i[show, edit, update]
 
   def index
+    @recipes = Recipe.all
   end
 
   def new
@@ -19,9 +21,29 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def edit
+    if current_recipe
+      render :edit
+    else
+      redirect_to root
+    end
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
   private
 
   def recipe_params
     params.require(:recipe).permit(:name, :yeild, :cook_time, :ingredients, :instructions)
+  end
+
+  def current_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
