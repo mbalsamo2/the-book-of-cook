c928f71608938ce5d68b72ff166bd7881fb8685a
mbalsamo <34141275+mbalsamo2@users.noreply.github.com>
2019-02-08 14:28:48 -0500

    deletes old db generated files, creates tests for user, creates password_digest for user, validates user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
-            format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+            format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
 
 end
