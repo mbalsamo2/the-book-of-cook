4541c1bc6bf2e320c637ea81f9a5121b8016264a
mbalsamo <34141275+mbalsamo2@users.noreply.github.com>
2019-02-18 14:37:55 -0500

    creates show page for user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :recipes
+  resources :users
 
   root 'welcome#home'
 end
