4a9bb763e4374783533a7223228c0e46b140f090
Mia Balsamo <mia.balsamo@gmail.com>
2020-05-30 11:33:05 -0400

    adds frontend react dependencies, able to start up server and navigate to homepage in newly created homepage/index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'homepage#index'
   get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :recipes
   resources :users
 
-  root 'welcome#home'
 end
